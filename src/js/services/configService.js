d38ea33817bff780a24855a270c0345a4743f043
Andy Phillipson <andy@bitpay.com>
2016-02-15 14:24:37 -0500

    Apply fix-default-config-polution to replace().

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -121,7 +121,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
   };
 
   root.replace = function(newOpts, cb) {
-    var config = defaultConfig;
+    var config = lodash.clone(defaultConfig);
     storageService.getConfig(function(err, oldOpts) {
       if (lodash.isString(oldOpts)) {
         oldOpts = JSON.parse(oldOpts);
