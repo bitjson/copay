df4aa237a3e6e712d2162627f28f7a3dc376c8e7
Andy Phillipson <andy@bitpay.com>
2016-03-21 15:12:19 -0400

    Disable accept disclaimer button on click (for visual feedback).

--- src/js/controllers/disclaimer.js
+++ src/js/controllers/disclaimer.js
@@ -2,8 +2,10 @@
 
 angular.module('copayApp.controllers').controller('disclaimerController', function($scope, $log, uxLanguage, profileService, go) {
   this.lang = uxLanguage.currentLanguage;
+  this.acceptInProgress = false;
 
   this.accept = function() {
+  	this.acceptInProgress = true;
     profileService.setDisclaimerAccepted(function(err) {
       if (err) $log.error(err);
       else go.walletHome();
